2968e252d1998cc95146e765094c339f1f895592
James Grams <jrg@pavdevco.com>
2021-06-29 08:22:02 -0400

    Incremental improvements

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -18,3 +18,66 @@ window.addEventListener("DOMContentLoaded", function() {
         observerItem.observe(el);
     });
 });
+
+// form submission
+var form = document.querySelector("form");
+if( form ) {
+    form.onsubmit = function(e) {
+        e.preventDefault();
+        var formButton = document.querySelector("form button");
+        formButton.classList.add("disabled");
+        var fields = ["name", "email", "message"];
+        var body = [];
+        for(var i=0; i<fields.length; i++) {
+            body.push(fields[i] + ": " + document.getElementById(fields[i]).value);
+        }
+        body = body.join("<br><br>");
+        makeRequest( "POST", "https://game103.net/ws/contact.php", { 
+            "email": "james@smartsteplavs.com",
+            "subject": "Smart-Step Lavs Form Submission",
+            "body": body
+        }, function() {
+            document.querySelector("#form-error").innerText = "Your message has been successfully submitted. We will be in contact soon.";
+        }, function() {
+            document.querySelector("#form-error").innerText = "An error has ocurred. Please try again later.";
+            formButton.classList.remove("disabled");
+        } );
+    }
+}
+
+// make request
+function makeRequest(type, url, parameters, callback, errorCallback) {
+    var parameterKeys = Object.keys(parameters);
+    var parameterArray = [];
+    for( var i=0; i<parameterKeys.length; i++ ) {
+        parameterArray.push( parameterKeys[i] + "=" + parameters[parameterKeys[i]] );
+    }
+
+    if( type == "GET" && parameterKeys.length ) {
+        url = url + "?" + parameterArray.join("&");
+    }
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open(type, url, true);
+
+    if( type == "POST" && parameterKeys.length ) {
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    } 
+
+    xhttp.onreadystatechange = function() {
+        if( this.readyState == 4 ) {
+            if( this.status == 200 ) {
+                if( callback ) { callback(this.responseText); }
+            }
+            else {
+                if( errorCallback ) { errorCallback(); }
+            }
+        }
+    }    
+    if( type == "POST" && parameterArray.length ) {
+        xhttp.send( parameterArray.join("&") );
+    }
+    else {
+        xhttp.send();
+    }
+}
